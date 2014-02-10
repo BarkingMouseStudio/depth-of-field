e30680013a8456376f4f7d5bf4014b200c52073a
jimfleming <jimmy.fleming@gmail.com>
2014-02-09 18:11:33 -0800

    Fixing in-editor DoF

--- Assets/Scripts/DepthOfField.cs
+++ Assets/Scripts/DepthOfField.cs
@@ -1,94 +1,77 @@
 using UnityEngine;
 using System.Collections;
 
-// [ExecuteInEditMode]
+[ExecuteInEditMode]
 [RequireComponent(typeof(Camera))]
 public class DepthOfField : MonoBehaviour {
-  protected internal RenderTexture targetTexture;
 
-  private RenderTexture grabTextureA;
-  private RenderTexture grabTextureB;
-  private RenderTexture grabTextureC;
-  private RenderTexture grabTextureD;
-
-  public float aberration = 0;
-  public float vignetting = 0;
-
-  [HideInInspector]
-  public Shader shader;
-  protected internal Material material;
   public Transform focus;
   public float aperture = 1;
 
-  public int captureBufferWidth = 512;
-  public int captureBufferHeight = 256;
+  [Range(2, 8)]
+  public int downsampleFactor = 4;
 
-  public bool useCheap = false;
+  [HideInInspector]
+  public Shader shader;
 
-  public void Awake() {
+  [HideInInspector]
+  public Material material;
+
+  RenderTexture CreateTexture(int width, int height) {
+    RenderTexture texture = RenderTexture.GetTemporary(width, height, 0, RenderTextureFormat.ARGB32);
+    texture.wrapMode = TextureWrapMode.Clamp;
+    texture.useMipMap = false;
+    texture.isPowerOfTwo = true;
+    texture.filterMode = FilterMode.Bilinear;
+    return texture;
+  }
+
+  void Awake() {
     material = new Material(shader);
-
-    int ajustedforSquareCaptureBufferWidth = captureBufferWidth;
-    int ajustedforSquareCaptureBufferHeight = captureBufferHeight;
-
-    targetTexture = new RenderTexture(Screen.width, Screen.height, 16, RenderTextureFormat.ARGB32);
-    targetTexture.wrapMode = TextureWrapMode.Clamp;
-    targetTexture.useMipMap = false;
-    targetTexture.isPowerOfTwo = false;
-    targetTexture.filterMode = FilterMode.Bilinear;
-    targetTexture.Create();
-
-    grabTextureD = new RenderTexture(captureBufferWidth / 2, captureBufferHeight / 2, 0, RenderTextureFormat.ARGB32);
-    grabTextureD.wrapMode = TextureWrapMode.Clamp;
-    grabTextureD.useMipMap = false;
-    targetTexture.isPowerOfTwo = true;
-    grabTextureD.filterMode = FilterMode.Bilinear;
-    grabTextureD.Create();
-    material.SetTexture("_BlurTexD", grabTextureD);
-
-    grabTextureA = new RenderTexture(captureBufferWidth, captureBufferHeight, 0, RenderTextureFormat.ARGB32);
-    grabTextureA.wrapMode = TextureWrapMode.Clamp;
-    grabTextureA.useMipMap = false;
-    targetTexture.isPowerOfTwo = true;
-    grabTextureA.filterMode = FilterMode.Bilinear;
-    grabTextureA.Create();
-    Shader.SetGlobalTexture("_CaptureTex", grabTextureA);
-
-    grabTextureB = new RenderTexture(ajustedforSquareCaptureBufferWidth / 2, ajustedforSquareCaptureBufferHeight / 2, 0, RenderTextureFormat.ARGB32);
-    grabTextureB.wrapMode = TextureWrapMode.Clamp;
-    grabTextureB.useMipMap = false;
-    targetTexture.isPowerOfTwo = true;
-    grabTextureB.filterMode = FilterMode.Bilinear;
-    grabTextureB.Create();
-    material.SetTexture("_BlurTexA", grabTextureB);
-
-    grabTextureC = new RenderTexture(ajustedforSquareCaptureBufferWidth / 4, ajustedforSquareCaptureBufferHeight / 4, 0, RenderTextureFormat.ARGB32);
-    grabTextureC.wrapMode = TextureWrapMode.Clamp;
-    grabTextureC.useMipMap = false;
-    targetTexture.isPowerOfTwo = true;
-    grabTextureC.filterMode = FilterMode.Bilinear;
-    grabTextureC.Create();
-    material.SetTexture("_BlurTexB", grabTextureC);
-
     camera.depthTextureMode = DepthTextureMode.None; // Explicitly disable depthmap
   }
 
   void OnRenderImage(RenderTexture src, RenderTexture dest) {
+    // Initialize textures
+    downsampleFactor = Mathf.Clamp(downsampleFactor, 2, 8);
+
+    int captureBufferWidth = Mathf.NextPowerOfTwo(Screen.width / downsampleFactor);
+    int captureBufferHeight = Mathf.NextPowerOfTwo(Screen.height / downsampleFactor);
+
+    if (captureBufferWidth > captureBufferHeight) {
+      captureBufferHeight = captureBufferWidth;
+    } else {
+      captureBufferWidth = captureBufferHeight;
+    }
+
+    RenderTexture grabTextureA = CreateTexture(captureBufferWidth / 2, captureBufferHeight / 2);
+    material.SetTexture("_GrabTextureA", grabTextureA);
+
+    RenderTexture grabTextureB = CreateTexture(captureBufferWidth / 4, captureBufferHeight / 4);
+    material.SetTexture("_GrabTextureB", grabTextureB);
+
+    RenderTexture grabTextureC = CreateTexture(captureBufferWidth / 2, captureBufferHeight / 2);
+    material.SetTexture("_GrabTextureC", grabTextureC);
+
+    // Setup material variables
     Shader.SetGlobalFloat("_DepthFar", Vector3.Distance(transform.position, focus.position));
     Shader.SetGlobalFloat("_DepthAperture", aperture);
 
-    material.SetFloat("_Aberration", aberration);
-    material.SetFloat("_Vignetting", vignetting);
-
+    // Blit textures
     grabTextureA.DiscardContents();
-    Graphics.Blit(src, grabTextureA, material, 5);
-    grabTextureB.DiscardContents();
-    Graphics.Blit(grabTextureA, grabTextureB, material, 2);
-    grabTextureC.DiscardContents();
-    Graphics.Blit(grabTextureB, grabTextureC, material, 2);
-    grabTextureD.DiscardContents();
-    Graphics.Blit(null, grabTextureD, material, 0);
+    Graphics.Blit(src, grabTextureA, material, 1);
 
-    Graphics.Blit(src, null, material, 3);
+    grabTextureB.DiscardContents();
+    Graphics.Blit(grabTextureA, grabTextureB, material, 1);
+
+    grabTextureC.DiscardContents();
+    Graphics.Blit(null, grabTextureC, material, 0);
+
+    Graphics.Blit(src, dest, material, 2);
+
+    // Cleanup
+    RenderTexture.ReleaseTemporary(grabTextureA);
+    RenderTexture.ReleaseTemporary(grabTextureB);
+    RenderTexture.ReleaseTemporary(grabTextureC);
   }
 }
